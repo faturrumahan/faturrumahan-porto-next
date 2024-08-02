a6e02a241875a39a1704b0a10615891e07a56dc7
Fathurrohman <faturrumahan@gmail.com>
2024-08-02 11:24:26 +0700

    init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const DashboardPage = () => {
+  return <Button>DashboardPage</Button>;
+};
+
+export default DashboardPage;
